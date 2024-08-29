6fde3d5ebc0876ea09bb8185cefd5d1a15387007
Amirul Islam <amirul.islam@bkash.com>
2024-08-30 00:17:20 +0600

    v2.10.1
    [*] added rabbitmq support

--- examples/rabbitmq.cpp
+++ examples/rabbitmq.cpp
@@ -5,9 +5,19 @@
 #include <axon/util.h>
 #include <axon/rabbit.h>
 
+struct fileinfo {
+
+	unsigned long index;
+	unsigned long total;
+	char filename[PATH_MAX];
+	char batchid[64];
+};
+
+bool running = false;
+
 namespace {
-std::function<void(int)> shutdown_handler;
-void signal_handler(int signal) { shutdown_handler(signal); }
+	std::function<void(int)> shutdown_handler;
+	void signal_handler(int signal) { running = false; shutdown_handler(signal); }
 }
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[], char* env[])
@@ -36,29 +46,79 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[], char* env[])
 			sid = parts[1];
 	}
 
-	std::string name = "hyperion";
-	int option = 0;
+	std::string vhost = "hyperion", queue = "test";
+	int option = 0, count = 10;
 
 	if (argc > 1) option = atoi(argv[1]);
+	if (argc > 2) queue = argv[2];
+	if (argc > 3) count = atoi(argv[3]);;
 
 	if (option == 0)
 	{
 		axon::queue::producer p;
-		p.connect("localhost", 5672, name, name, name);
-		for (int i = 0; i < 10; i++) p.push(std::string("blah") + std::to_string(i), name, name);
+		// p.connect("localhost", 5672, vhost, "hyperion", "hyperion");
+		p.connect("10.96.38.223", 30000, vhost, "hyperion-user", "hyperion-user");
+		
+		p.make_queue(queue);
+		
+		for (int i = 0; i < count; i++)
+		{
+			char fn[PATH_MAX];
+			sprintf(fn, "filename=%d", i);
+			struct fileinfo fi;
+			fi.total=0;
+			fi.index=0;
+			strncpy(fi.batchid, fn, 64);
+			sprintf(fi.filename, "%s", fn);
+
+			p.push("amq.direct", queue,
+				static_cast<char*>(static_cast<void*>(&fi)),
+				sizeof(fi),
+				axon::queue::encoding::binary
+			);
+			// p.push("amq.direct", queue, std::string("blah") + std::to_string(i)); // can be used to sent line of text!
+		}
 		p.close();
 	}
 	else
 	{
 		axon::queue::consumer c;
+		std::string text;
 
-		// signal(SIGINT, signal_handler);
-		// shutdown_handler = [&](int signal) { c.stop(); };
+		signal(SIGINT, signal_handler);
+		shutdown_handler = [&](int signal) { printf("signal %d handler!\n", signal); c.stop(); };
 
-		c.connect("localhost", 5672, name, name, name);
-		c.attach(name, name, name);
-		c.start([&](std::string txt) { std::cout<<name<<": "<<txt<<std::endl; });
-		sleep(3);
+		// c.connect("localhost", 5672, vhost, "hyperion", "hyperion");
+		c.connect("10.96.38.223", 30000, vhost, "hyperion-user", "hyperion-user");
+
+		c.make_queue(queue);
+		// c.attach(queue, "amq.direct", queue); // if we want to use pop()
+
+		running = true;
+
+		// c.start([&](std::string txt) { std::cout<<queue<<": "<<txt<<std::endl; }); // use as lambda
+
+		// axon::queue::envelope e = c.pop();
+		// if (e.size > 0) std::cout<<e.ss.str()<<std::endl;
+		// sleep(1);
+
+		while (running) {
+			// axon::queue::envelope e = c.pop();
+			axon::queue::envelope e = c.get(queue);
+
+			if (e.enc == axon::queue::encoding::text)
+				std::cout<<e.ss.str()<<std::endl;
+			else if (e.enc == axon::queue::encoding::binary)
+			{
+				struct fileinfo fio;
+
+				std::string blah = e.ss.str();
+				memcpy(&fio, blah.c_str(), sizeof(fio));
+				std::cout<<fio.filename<<std::endl;
+			}
+		}
+
+		c.delete_queue(queue);
 		c.stop();
 		c.close();
 	}
--- extras/notes.md
+++ extras/notes.md
@@ -1,3 +1,5 @@
+## howto keytab
+
 $ ktutil
 addent -password -p amirul.islam -k 1 -e aes256-cts-hmac-sha1-96
 wkt amirul.islam.kt
@@ -5,3 +7,23 @@ wkt amirul.islam.kt
 $ kinit -kt amirul.islam.kt amirul.islam@BKASH.COM
 
 
+## rabbit mq init
+
+rabbitmqctl add_user hyperion hyperion
+// rabbitmqctl set_user_tags hyperion administrator
+// rabbitmqctl set_permissions -p / hyperion ".*" ".*" ".*"
+
+rabbitmqctl add_vhost hyperion
+// rabbitmqctl set_permissions -p hyperion guest ".*" ".*" ".*"
+rabbitmqctl set_permissions -p hyperion hyperion ".*" ".*" ".*"
+
+rabbitmqadmin -H localhost -u hyperion -p hyperion list vhosts
+rabbitmqadmin -H localhost -u hyperion -p hyperion declare exchange --vhost=hyperion name=hyperion type=direct
+rabbitmqadmin -H localhost -u hyperion -p hyperion declare queue --vhost=hyperion name=hyperion durable=true
+rabbitmqadmin -H localhost -u hyperion -p hyperion declare queue --vhost=hyperion name=completed durable=true
+rabbitmqadmin -H localhost -u hyperion -p hyperion --vhost="hyperion" declare binding source="hyperion" destination_type="queue" destination="hyperion" routing_key="hyperion"
+
+
+### reset
+rabbitmqctl delete_vhost hyperion
+rabbitmqctl delete_user hyperion
